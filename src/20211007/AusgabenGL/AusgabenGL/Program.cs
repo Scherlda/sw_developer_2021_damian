2d00911b1e7b7585a078d1a8b1f96d5ff92cc1b6
Damian <scherlda@outlook.com>
2021-10-07 21:29:34 +0200

    Anatomie C# Applikation

---
+++ src/20211007/AusgabenGL/AusgabenGL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AusgabenGL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heute ist der " + DateTime.Now.ToString());
+            Console.WriteLine("Schönen Tag!");
+        }
+    }
+}
