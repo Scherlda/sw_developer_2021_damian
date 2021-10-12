76376a3f7e04a7c08344976a2ad22eb5bf9cd8ff
Damian <scherlda@outlook.com>
2021-10-12 19:10:13 +0200

    Ausgabe an verschiedenen Positionen

---
+++ src/20211012/Ausgaben_GL/Ausgaben_GL/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ausgaben_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Hi, schön das du da bist");
+            //Console.WriteLine("Mein Alter: " + 150);
+            //Console.WriteLine("Ich bin {0} Jahre alt", 150);
+
+            //Console.WriteLine("Hallo\nzusammen");
+            //Console.WriteLine("\tHallo\nzusammen");
+            //Console.WriteLine("Hallo\n\tzusammen");
+            //Console.WriteLine("\t\tHallo\nzusammen\a");
+
+            Console.SetCursorPosition(10, 2);
+            Console.WriteLine("Name: Damian");
+            Console.SetCursorPosition(2, 3);
+            Console.WriteLine("Geburts Jahr: 2000");
+            Console.SetCursorPosition(0, 5);
+            Console.WriteLine("Das halte ich von C#:");
+            Console.SetCursorPosition(8, 6);
+            Console.WriteLine("C# ist eine tolle Sache!");
+            
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
