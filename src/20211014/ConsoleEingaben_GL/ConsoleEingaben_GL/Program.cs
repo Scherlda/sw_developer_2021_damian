a41b3217db0c1662c127b09dcfaa356403c2280c
Damian <scherlda@outlook.com>
2021-10-14 19:31:31 +0200

    Fertigstellung Aufgabe Rechteck

---
+++ src/20211014/ConsoleEingaben_GL/ConsoleEingaben_GL/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEingaben_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Elegantere Lösung um den string leer zu lassen. Ist etwas mehr Speicheroptimiert.
+            string name = string.Empty;
+            string geburtsJahrEingabe = string.Empty;
+            int geburtsJahr = 0;
+            int alter = 0;
+            bool eingabePasst = false;
+
+            Console.Write("Bitte Name eingeben: ");
+            name = Console.ReadLine();
+
+            Console.Write("\nHallo {0}, wie geht's\n", name);
+           
+            Console.Write("{0}, bitte gib dein Geburtsjahr ein: ", name);
+            geburtsJahrEingabe = Console.ReadLine();            
+
+            //Konvertierung Zeichenkette nach int
+            geburtsJahr = int.Parse(geburtsJahrEingabe);
+
+            //Alter von User berechnen
+            alter = DateTime.Now.Year - geburtsJahr;
+
+            Console.WriteLine("{0}, du bist {1} Jahre alt.",name, alter);
+
+        }
+    }
+}
---
+++ src/20211014/Rechtecksberechnung/Rechtecksberechnung/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechtecksberechnung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Aufgabe
+             *   Schreiben Sie eine Applikation, mit der der Umfang eines beliebigen Rechtecks berechnet werden kann.
+             *   Dazu soll die Applikation die Seitenlänge vom User einlesen und anschliessend den Umfang berechnen
+             *   Ausgabe Ergebnis (Umfang) inklusive den Seitenlängen, schön formatiert           
+             */
+
+            //Variablen
+            string aEingabe = string.Empty;
+            int a = 0;
+            string bEingabe = string.Empty;
+            int b = 0;
+            int u = 0;
+
+            //User Ineraktion
+            Console.WriteLine("Hallo, mit diesem Konsolen Programm kannst du den Umfang eines Rechtecks berechnen");
+
+            Console.Write("Bitte gib die Seite a ein: ");
+            aEingabe = Console.ReadLine();
+          
+            Console.Write("Bitte gib die Seite b ein: ");
+            bEingabe = Console.ReadLine();
+
+            //Konvertierung + Berechnung des Rechecks
+            a = int.Parse(aEingabe);
+            b = int.Parse(bEingabe);
+            u = 2*(a+b);
+
+            //Ausgabe
+            Console.Clear();
+            Console.Write("Die Seitenlänge a ist: {0}\nDie Seitenlänge b ist: {1}\n\n", a, b);
+            Console.Write("Umgabeberechnung: u = 2 * (a + b)\n\nDer Umfang ist: {0}\n", u);
+
+
+        }
+    }
+}
