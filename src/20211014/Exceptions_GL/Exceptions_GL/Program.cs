3543b1d9a0e0a5899d87391e432669bb405f7cad
Damian <scherlda@outlook.com>
2021-10-14 21:27:09 +0200

    Exeptions Grundlagen

---
+++ src/20211014/Exceptions_GL/Exceptions_GL/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int geburtsJahr = 0;
+            int alter = 0;
+            bool isInputValid = false;
+
+            Console.Write("Bitte Geburtsjahr eingeben: ");
+
+            try //Code in diesem Block ist nicht perfomant
+            {
+                geburtsJahr = int.Parse(Console.ReadLine()); //Passiert hier ein Fehler dann wird das Programm auch hier beendet und der bool wird nie auf true gesetzt
+                isInputValid = true;
+            }
+            catch
+            {
+                Console.WriteLine("Ups!, leider ist etwas schief gegangen!");
+                isInputValid = false;
+            }
+
+            if (isInputValid == true)
+            {
+                alter = DateTime.Now.Year - geburtsJahr;
+
+                Console.WriteLine("Du bist {0} Jahre alt", alter);
+            }
+
+
+        }
+    }
+}
