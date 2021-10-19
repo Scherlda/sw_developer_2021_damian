ece2319033cf29eeb251b2c25fc8fd665460d6dc
Damian <scherlda@outlook.com>
2021-10-19 20:55:28 +0200

    Grundlagen interrationen

--- src/20211019/GL_while/GL_while/Program.cs
+++ src/20211019/GL_while/GL_while/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GL_while
@@ -10,6 +11,22 @@ namespace GL_while
     {
         static void Main(string[] args)
         {
+            int zahl = 4;
+
+            //Ist der Wert bereits erfüllt do wird es nicht ausgeführt
+            while(zahl < DateTime.Now.Second)
+            {
+                Console.WriteLine(DateTime.Now.Second);
+                Thread.Sleep(1000);
+            }
+
+            //Wird zuerst ausgeführt und erst dann wird geprüft ob der Wert erfüllt ist.
+            do
+            {
+                Console.WriteLine($"do-while: {DateTime.Now.Second}");
+                Thread.Sleep(1000);
+
+            } while (zahl < DateTime.Now.Second);
 
 
         }
