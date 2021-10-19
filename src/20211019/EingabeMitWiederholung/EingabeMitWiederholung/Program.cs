78dfb4a63f41ea0b94f8a9e685d5aeaa047e990d
Damian <scherlda@outlook.com>
2021-10-19 21:26:41 +0200

    Methoden Grundlagen mit void

---
+++ src/20211019/EingabeMitWiederholung/EingabeMitWiederholung/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EingabeMitWiederholung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 0;
+            bool isInputValid = false;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Bitte das Alter eingeben:");
+                    alter = int.Parse(Console.ReadLine());
+                    isInputValid = true;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Ungültige Eingabe.");
+                    isInputValid = false;
+                }
+            }
+            while (!isInputValid);
+
+            Console.WriteLine($"Sie sind {alter} Jahre alt");
+        }
+    }
+}
---
+++ src/20211019/Methoden_GL/Methoden_GL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methoden_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterHelloWorld();
+
+            string name = "Gandalf";
+
+            PrintMessage(name);
+            PrintMessage(Console.ReadLine());
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //Signatur
+        //RückgabeTyp MethodenBezeichnung(Parameter)
+        static void PrinterHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
