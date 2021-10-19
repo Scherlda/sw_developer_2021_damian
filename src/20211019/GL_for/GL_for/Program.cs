1702be016d95bd383b90bc72196e04db64a75a32
Damian <scherlda@outlook.com>
2021-10-19 20:40:07 +0200

    for schleife

---
+++ src/20211019/GL_for/GL_for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GL_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for wird verwendet wenn die Anzahl der Wiederholungen bekannt ist
+            //Initioalisierung; Abbruchbedingung; Reinitialisierung
+             for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ausgabe mit {i}");
+            }
+        }
+    }
+}
