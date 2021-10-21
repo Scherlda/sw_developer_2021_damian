f5bee279dcc057eb28e756312e40a1bc50f91cab
Damian <scherlda@outlook.com>
2021-10-21 20:58:42 +0200

    Hausaufgabe

---
+++ src/20211021/Buchverwaltung_v1/Buchverwaltung_v1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buchverwaltung_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { /*
+           * Die Applikation soll Buchdatn erfassen.
+           * Folgende Daten sollen dabei berücksichtigt werden:
+           * 
+           *  - Titel
+           *  - Autor
+           *  - Erscheinungsjahr
+           *  - IBAN
+           *  - Preis
+           *  
+           *  Dabei gilt:
+           *  
+           *  1. Verwende und schreibe Methoden!
+           *  2. Realisiere fehlertolerante Eingaben!
+           *  
+           *  3. Perisistiere (heisst speichern) die Buchdaten in einer Datei, welhe so heisst wie der Titel
+           *     Bsp. Buch Titel = "Die unendliche Geschichte" => DieunendlicheGeschichte.book
+           *  
+           *  4. Bauae das Program so um, dass der User beliebig viele Bücher eingeben kann und jedes 
+           *     Buch nach der Eingabe automatisch abgespeichert wird. (Gibt es das Buch bereits, soll die Datei )
+           *  
+           */
+
+
+
+            Console.WriteLine(new string('#', Console.WindowWidth - 1));
+
+
+
+        }
+    }
+}
