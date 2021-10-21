bbb4c9996aded7d8bf0d8f31d544bc78aefa6a9f
Damian@Notebook <scherlda@outlook.com>
2021-10-21 17:43:48 +0200

    übung

---
+++ src/20211021/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string headerText = "Test Program";
+            int xPos = Console.WindowWidth / 2 - headerText.Length / 2;
+
+            Console.WriteLine(new string('#', Console.WindowWidth));
+            Console.SetCursorPosition(2,xPos);
+            Console.WriteLine(new String('#', Console.WindowWidth));
+
+
+
+        }
+    }
+}
