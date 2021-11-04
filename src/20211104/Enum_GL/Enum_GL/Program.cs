32493cb10a9327adaaad71a6b521007eee295f14
Damian <scherlda@outlook.com>
2021-11-04 21:11:05 +0100

    übungen Enum

---
+++ src/20211104/Enum_GL/Enum_GL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaration
+            PowerState status;
+
+            //Initialisierung
+            status = PowerState.Hibernate;
+
+            Console.WriteLine($"Wert von Status: {status}");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
