7ad6d4034322f279841362b75e12d216863c8c0a
Damian <scherlda@outlook.com>
2021-11-09 19:20:32 +0100

    Finish basic class dsign with problems(!)

--- src/20211104/Klassen_GL/Klassen_GL/Mitarbeiter.cs
+++ src/20211104/Klassen_GL/Klassen_GL/Mitarbeiter.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wifi.ConsoleTools.Misc;
 
 namespace Klassen_GL
 {
     class Mitarbeiter
     {
+        public Guid ID; //
         public string Name;
+        public Gender Geschlecht;
 
         public void ChangeName(string newName)
         {
@@ -16,6 +19,13 @@ namespace Klassen_GL
             {
                 Name = newName;
             }
+        }
+
+        public void DisplayInfos()
+        {
+            Console.WriteLine($"ID:     [{ID}]");
+            Console.WriteLine($"Name    {Name}");
+            Console.WriteLine($"Gender: {Geschlecht}");
         }
     }
 }
--- src/20211104/Klassen_GL/Klassen_GL/Program.cs
+++ src/20211104/Klassen_GL/Klassen_GL/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wifi.ConsoleTools.Misc;
 
 namespace Klassen_GL
 {
@@ -14,6 +15,12 @@ namespace Klassen_GL
             Mitarbeiter ma = new Mitarbeiter();
 
             //Klasse / Object = Instanz
+            ma.Name = "Gandalf";
+            ma.Geschlecht = Gender.Male;
+            ma.ChangeName("Gandalf der Graue");
+
+            ma.DisplayInfos();
+
             //Klasse ist ein Datentyp, im oberen Beispiel die Klasse Mitarbeiter
             // ma ist ein Object/Instanz
             
