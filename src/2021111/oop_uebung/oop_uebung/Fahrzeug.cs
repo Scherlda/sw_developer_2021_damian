7edc69df691d3026868c7e72cde9fdb93bbf5662
Damian <scherlda@outlook.com>
2021-11-11 16:32:33 +0100

    übung

---
+++ src/2021111/oop_uebung/oop_uebung/Fahrzeug.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_uebung
+{
+    class Fahrzeug
+    {
+        private int _currentSpeed;
+        private int _maxSpeed;
+        private string _manufacturer;
+        private string _modelName;
+               
+
+        public int CurremtSpeed
+        {
+            get { return _currentSpeed; }
+        }
+
+        public int MaxSpeed
+        {
+            get { return _maxSpeed; }
+        }
+
+        public string Manufacturer
+        {
+            get { return _manufacturer; }
+        }
+
+        public string ModelName
+        {
+            get { return _modelName; }
+        }
+
+    }
+}
