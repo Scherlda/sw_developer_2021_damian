482f3af0eb504d72b0112c96e818e9d1e74762d7
Damian <scherlda@outlook.com>
2021-11-18 21:33:28 +0100

    Generics

---
+++ src/20211118/Generics_GL/Generics_GL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elementCount = 50;
+            var initValue = -1;
+
+            int[] meineZahlen;
+
+            meineZahlen = InitArray<int>(5, 0);
+            var namen = InitArray<string>(5, "test");
+            var datumswert = InitArray<DateTime>(10, DateTime.Now);
+        }
+
+        //Generische Methode
+        static T[] InitArray<T>(int elementCount, T initValue)              //T steht für Type, aber hier könnte alles stehen ob T, A, Ente oder sonst was. Da wo nun T steht kann jeder Datentyp übergeben werden.
+        {
+            T[] tmpArray = new T[elementCount];
+            for (int i = 0; i < tmpArray.Length; i++)
+            {
+                tmpArray[i] = initValue;
+            }
+
+            return tmpArray;
+        }
+
+
+
+
+        private static int[] InitArray(int elementCount, int initValue)
+        {
+            int[] tmpArray = new int[elementCount];
+
+            for (int i = 0; i < tmpArray.Length; i++)
+            {
+                tmpArray[i] = initValue;
+            }
+            return tmpArray;
+        }
+    }
+}
