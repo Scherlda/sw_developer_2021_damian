eefe8fa5ca74909ec9504b57a2d4da3abb0712a6
Damian <scherlda@outlook.com>
2021-11-23 21:43:26 +0100

    Datentypen und Forms

---
+++ src/20211123/DatenTypArten/DatenTypArten/Buch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatenTypArten
+{
+    public class Buch
+    {
+        public string Titel;
+        public decimal Preis;
+    }
+}
