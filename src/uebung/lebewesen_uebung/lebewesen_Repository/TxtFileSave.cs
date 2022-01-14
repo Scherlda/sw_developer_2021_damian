d638f9d15d6d29fc21caa0172c78bc60c2df74d2
Damian <scherlda@outlook.com>
2022-01-14 17:20:59 +0100

    export

---
+++ src/uebung/lebewesen_uebung/lebewesen_Repository/TxtFileSave.cs
@@ -0,0 +1,31 @@
+﻿using lebewesen_Item;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lebewesen_Repository
+{
+    public class TxtFileSave
+    {
+        public bool SaveToTxt(List<Lebewesen> dataToExport, string filePath)
+        {
+            if (dataToExport == null || string.IsNullOrEmpty(filePath)) return false;
+            
+
+            using (StreamWriter sw = new StreamWriter(filePath, false))
+            {
+                foreach (Lebewesen lebewesen in dataToExport)
+                {
+                    sw.WriteLine(lebewesen);
+                }
+                
+            }
+            
+            return true;
+
+        }
+    }
+}
--- src/uebung/lebewesen_uebung/lebewesen_uebung/Program.cs
+++ src/uebung/lebewesen_uebung/lebewesen_uebung/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using lebewesen_Item;
+using lebewesen_Repository;
 
 namespace lebewesen_main
 {
@@ -11,16 +12,20 @@ namespace lebewesen_main
     {
         static void Main(string[] args)
         {
-            List<ILebewesen> lebewesenList = new List<ILebewesen>();
+            List<Lebewesen> lebewesenList = new List<Lebewesen>();
 
             lebewesenList.Add(new Hund(4, 50));
             lebewesenList.Add(new Katze(4, 30));
             lebewesenList.Add(new Mensch(2, 170));
+            lebewesenList.Add(new Hund(4, 40));
 
             foreach (var item in lebewesenList)
             {
                 Console.WriteLine(item.GibLaut());
             }
+
+            TxtFileSave save = new TxtFileSave();
+            save.SaveToTxt(lebewesenList, @"C:\opt\test.txt");
 
         }
     }
